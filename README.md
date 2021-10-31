7b86381836f634476bf872ba5c2fb175d5999634
infoviw <sadviwichi@gmail.com>
2021-10-31 16:08:09 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# A simple TicTacToe using React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Head oner to [ React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,144 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Reset({ status }) {
+  return (
+    <>
+      <div className="gameContainer">
+        <div className="res">
+          <h1>{status}</h1>
+          <br />
+          <br />
+          <img
+            src="./giphy.gif"
+            alt="pic"
+            onClick={() => {
+              window.location.reload();
+            }}
+          />
+          <br />
+          <br />
+        </div>
+      </div>
+      <h3 style={{ textAlign: "center", color: "white" }}>
+        Tap SquidWard to play again
+      </h3>
+    </>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Square({ value, onClick }) {
+  return (
+    <button className="box boxtext" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function Game({ winner, getStatus, renderSquare }) {
+  return (
+    <>
+      <div className="gameInfo">
+        <h1>Let's play TicTacToe</h1>
+      </div>
+      <div className="gameContainer">
+        <div className="container">
+          <div className="line"></div>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="info">
+        <span>{getStatus()}</span>
+      </div>
+      
+    </>
+  );
+}
+
+function Main() {
+  const [next, setNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  const who = next ? "X" : "O";
+
+  function getStatus() {
+    if (winner) {
+      return winner + " beat " + who;
+    } else if (isBoardFull(squares)) {
+      return "That's a draw match!";
+    } else {
+      return "Next turn " + who;
+    }
+  }
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const newSquares = squares.slice();
+          newSquares[i] = who;
+          setSquares(newSquares);
+          setNext(!next);
+        }}
+      />
+    );
+  }
+
+  return (
+    <>
+      {winner || isBoardFull(squares) ? (
+        <Reset status={getStatus()} />
+      ) : (
+        <Game
+          winner={winner}
+          getStatus={getStatus}
+          renderSquare={renderSquare}
+        />
+      )}
+    </>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
